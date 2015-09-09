8d84a611c658320c533912e637a8a99cc2ba5852
Adam Chainz <adam@adamj.eu>
2015-09-09 11:36:46 +0100

    Add Django migration for creating Switch

---
+++ gargoyle/migrations/0001_initial.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import django.utils.timezone
+import jsonfield.fields
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Switch',
+            fields=[
+                ('key', models.CharField(primary_key=True, serialize=False, max_length=64)),
+                ('value', jsonfield.fields.JSONField(default=b'{}')),
+                ('label', models.CharField(max_length=64, null=True)),
+                ('date_created', models.DateTimeField(default=django.utils.timezone.now)),
+                ('date_modified', models.DateTimeField(auto_now=True)),
+                ('description', models.TextField(null=True)),
+                ('status', models.PositiveSmallIntegerField(
+                    default=1,
+                    choices=[(1, b'Disabled'), (2, b'Selective'), (3, b'Global'), (4, b'Inherit')]
+                )),
+            ],
+            options={
+                'verbose_name_plural': 'switches',
+                'verbose_name': 'switch',
+                'permissions': (('can_view', 'Can view'),),
+            },
+        ),
+    ]
