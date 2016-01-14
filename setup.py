96bfb53ee943188fed4af68084e2064d4328712f
Adam Chainz <adam@adamj.eu>
2016-01-14 10:50:11 +0000

    setup.py - add 'publish' command

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import os
 import re
+import sys
 
 from setuptools import setup, find_packages
 
@@ -12,6 +13,24 @@ def get_version(package):
     init_py = open(os.path.join(package, '__init__.py')).read()
     return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
 
+version = get_version('gargoyle')
+
+
+if sys.argv[-1] == 'publish':
+    if os.system("pip freeze | grep wheel"):
+        print("wheel not installed.\nUse `pip install wheel`.\nExiting.")
+        sys.exit()
+    if os.system("pip freeze | grep twine"):
+        print("twine not installed.\nUse `pip install twine`.\nExiting.")
+        sys.exit()
+    os.system("rm -rf .eggs/ build/ dist/")
+    os.system("python setup.py sdist bdist_wheel")
+    os.system("twine upload dist/*")
+    print("You probably want to also tag the version now:")
+    print("  git tag -a v%s -m 'Version %s'" % (version, version))
+    print("  git push --tags")
+    sys.exit()
+
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
@@ -20,7 +39,7 @@ with open('HISTORY.rst') as history_file:
 
 setup(
     name='gargoyle-yplan',
-    version=get_version('gargoyle'),
+    version=version,
     author='DISQUS',
     author_email='opensource@disqus.com',
     maintainer='YPlan',
