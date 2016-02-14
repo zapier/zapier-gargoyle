zapier/zapier-gargoyle

commit 523c25807d6e4b2d356580e515258d67a0794d95

Move autodiscovery to AppConfig.ready
Fixes #10. Makes installation simpler and autodiscovery more predictable.

gargoyle/__init__.py
@@ -5,12 +5,16 @@ gargoyle
 :copyright: (c) 2010 DISQUS.
 :license: Apache License 2.0, see LICENSE for more details.
 """
+from django.utils.module_loading import autodiscover_modules
+
 from gargoyle.manager import gargoyle
 
 __version__ = '1.2.0'
 VERSION = __version__  # old version compat
 
 __all__ = ('gargoyle', 'autodiscover', '__version__', 'VERSION')
+
+default_app_config = 'gargoyle.apps.GargoyleAppConfig'
 
 
 def autodiscover():
@@ -19,21 +23,4 @@ def autodiscover():
     not present. This forces an import on them to register any gargoyle bits they
     may want.
     """
-    import copy
-    from django.conf import settings
-
-    from importlib import import_module
-
-    for app in settings.INSTALLED_APPS:
-        # Attempt to import the app's gargoyle module.
-        before_import_registry = copy.copy(gargoyle._registry)
-        try:
-            import_module('%s.gargoyle' % app)
-        except:
-            # Reset the model registry to the state before the last import as
-            # this import will have to reoccur on the next request and this
-            # could raise NotRegistered and AlreadyRegistered exceptions
-            gargoyle._registry = before_import_registry
-
-    # load builtins
-    __import__('gargoyle.builtins')
+    autodiscover_modules('gargoyle')

gargoyle/apps.py
@@ -1,0 +1,9 @@
+from django.apps import AppConfig
+
+
+class GargoyleAppConfig(AppConfig):
+    name = 'gargoyle'
+    verbose_name = 'Gargoyle'
+
+    def ready(self):
+        self.module.autodiscover()

gargoyle/nexus_modules.py
@@ -15,14 +15,12 @@ from django.conf.urls import patterns, url
 from django.http import HttpResponse, HttpResponseNotFound
 from django.utils import six
 
-from gargoyle import autodiscover, gargoyle, signals
+from gargoyle import gargoyle, signals
 from gargoyle.conditions import ValidationError
 from gargoyle.helpers import dumps
 from gargoyle.models import DISABLED, Switch
 
 GARGOYLE_ROOT = os.path.dirname(__file__)
-
-autodiscover()
 
 logger = logging.getLogger('gargoyle.switches')
 

commit 14c31307fd31631ecce0378aedbef95cec8531f2

Fix autodiscovery
523c25807d6e4b2d356580e515258d67a0794d95 broke autodiscovery for applications
that do not import `gargoyle.builtins` anywhere themselves, as it removed the
line that imported them.

gargoyle/__init__.py
@@ -23,4 +23,5 @@ def autodiscover():
     not present. This forces an import on them to register any gargoyle bits they
     may want.
     """
+    import gargoyle.builtins  # noqa
     autodiscover_modules('gargoyle')