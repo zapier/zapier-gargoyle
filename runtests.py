4f2ec87e795b659417dca7711a08bfdbd560f3e1
Adam Chainz <adam@adamj.eu>
2016-03-24 21:02:17 +0000

    Move isort config from .editorconfig to setup.cfg

--- runtests.py
+++ runtests.py
@@ -46,7 +46,7 @@ def run_flake8():
     print('Running flake8 code linting')
     ret = subprocess.call([
         'flake8',
-        '--exclude=.svn,CVS,.bzr,.hg,.git,__pycache__,.tox,south_migrations',
+        '--exclude=.svn,CVS,.bzr,.hg,.git,__pycache__,.tox',
         'gargoyle', 'tests'
     ])
     print('flake8 failed' if ret else 'flake8 passed')
